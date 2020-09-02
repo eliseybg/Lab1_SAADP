db96addee77baeeb219549bfd3a0f9a266c6c6e1
eliseybg <bgs6544@gmail.com>
2020-09-02 22:05:55 +0300

    lab 1 created

--- src/List/List.java
+++ src/List/List.java
@@ -4,43 +4,64 @@ public class List {
     private Node startNode = new Node(null, null, null);
 
     public List(String line) {
-        if (line == null || line.equals(""))
-            return;
+        line = line.substring(line.indexOf("=") + 1);
+//        перед каждым минусом, кроме первого, добавить знак +,
+//        чтоб через регуларное выражение поделить строку
         int searchMinusFrom = line.indexOf("=") + 2;
         while (line.indexOf("-", searchMinusFrom) != -1) {
             line = line.substring(0, line.indexOf("-", searchMinusFrom)) + "+"
                     + line.substring(line.indexOf("-", searchMinusFrom));
             searchMinusFrom = line.indexOf("-", searchMinusFrom) + 1;
         }
-        String[] symbols = line.split("(x\\^)|[+]");
+
+        String[] symbols = line.split("(x\\^)|(x)|[+]");
 
         for (int i = 0; i < symbols.length; i += 2) {
             try {
-                add(Integer.parseInt(symbols[i + 1]), Integer.parseInt(symbols[i]));
+                int num1 = switch (symbols[i + 1]) {
+                    case "" -> 1; // если у х нет степени
+                    default -> Integer.parseInt(symbols[i + 1]);
+                };
+                int num2 = switch (symbols[i]) {
+                    case "" -> 1; // если у х нет степени
+                    case "-" -> -1; // если перед х будет стоять знак -
+                    default -> Integer.parseInt(symbols[i]);
+                };
+                add(num1, num2);
+//                вылетит исключение об выходе из массива, если будет стоять простое число,
+//                потому что отсутствует х, а значит и в строке нет коэффициента при х
             } catch (ArrayIndexOutOfBoundsException e) {
                 add(0, Integer.parseInt(symbols[i]));
             }
         }
     }
 
+    public List() {
+
+    }
+
     public static void Add(List firstList, List resultList, List secondList) {
         resultList.clear();
         Node nodeForFirstList = firstList.startNode;
-        firstList:
         while (nodeForFirstList.nextNode != null) {
             nodeForFirstList = nodeForFirstList.nextNode;
-            Node nodeForSecondList = secondList.startNode;
-            while (nodeForSecondList.nextNode != null) {
-                nodeForSecondList = nodeForSecondList.nextNode;
-                if (nodeForFirstList.degree.equals(nodeForSecondList.degree)) {
-                    if (nodeForFirstList.ratio + nodeForSecondList.ratio != 0)
-                        resultList.add(nodeForFirstList.degree, nodeForFirstList.ratio + nodeForSecondList.ratio);
-                    continue firstList;
-                }
-            }
             resultList.add(nodeForFirstList.degree, nodeForFirstList.ratio);
         }
-
+        Node nodeForSecondList = secondList.startNode;
+        resultList:
+        while (nodeForSecondList.nextNode != null) {
+            nodeForSecondList = nodeForSecondList.nextNode;
+            Node nodeForResultList = resultList.startNode;
+            while (nodeForResultList.nextNode != null) {
+                nodeForResultList = nodeForResultList.nextNode;
+                if (nodeForSecondList.degree.equals(nodeForResultList.degree)) {
+                    if (nodeForSecondList.ratio + nodeForResultList.ratio != 0)
+                        nodeForResultList.ratio = nodeForSecondList.ratio + nodeForResultList.ratio;
+                    continue resultList;
+                }
+            }
+            resultList.add(nodeForSecondList.degree, nodeForSecondList.ratio);
+        }
     }
 
     public static int Meaning(List list, int x) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
-public class Main {
-    
-    public static void main(String[] args) {
+import List.List;
 
+public class Main {
+    private static String example1 = "-5x^6+3x^2-2x+7";
+    private static String example2 = "x^3-x^2+7";
+    private static String example3 = "-2x^3-3x^2+x";
+    private static String example4 = "-x+7";
+    private static String example5 = "5";
+
+    public static void main(String[] args) {
+        List list1 = new List(example1);
+        System.out.println("list1    " + list1);
+
+        List list2 = new List(example2);
+        System.out.println("list2    " + list2);
+
+        System.out.println("list1 in point 1    " + List.Meaning(list1, 1));
+        System.out.println("list2 in point 0    " + List.Meaning(list2, 0));
+
+        System.out.println("list1 == list1    " + List.Equality(list1, list1));
+        System.out.println("list1 == list2    " + List.Equality(list1, list2));
+
+        List sum = new List();
+        List.Add(list1, sum, list2);
+
+        System.out.println("sum    " + sum);
     }
+
 }
